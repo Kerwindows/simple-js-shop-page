fa0f3d447f14f13c0a2023eb1e42e9f754886015
Kerwin <kerjemtec@gmail.com>
2020-03-20 20:42:49 +0000

    Update css-columns.php

--- css-columns.php
+++ css-columns.php
@@ -5,7 +5,7 @@
     Description: Keep All Divi Desktop layouts on Moble Column Layout 
     Author: Kerwin Thompson
     Author URI: http://www.kerjemtec.com
-    Version: 1.3.0
+    Version: 1.5.0
 */
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
