93fdecb8d46c13676b00a13a19fb853e97b21787
Kerwin <kerjemtec@gmail.com>
2020-06-05 18:20:02 +0000

    Upload New File

---
+++ thankyou.php
@@ -0,0 +1,28 @@
+<?php
+
+// loading the CSS
+	wp_enqueue_style('css-columns-style', plugins_url( 'style.css', __FILE__ ) );
+
+?>
+<div id="pluginwrap_k">
+	
+	
+	<div class="row_k">
+	  <div class="column_k">
+	  <h2>Thank you</h2>
+	  <p>Thank you for purchasing my plugin</p>
+	  
+	  </div>
+	  <div class="column_k">
+	    <h2>Updates</h2>
+	    <p>For updates...</p>
+	  </div>
+	  <div class="column_k" >
+	    <h2>Issues?</h2>
+	    <p>If you are experiencing any issues after going through the tutorial, or if you notice any errors, please contact me via my email handle, kerwin@kerjemtec.com.</p>
+	  </div>
+	</div>
+
+</div>
+
+</div>
