a72229bff3b13a9fc201a3760760d568fe43f0b4
Kerwin <kerjemtec@gmail.com>
2020-06-05 13:01:52 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 /*
- * Here your scripts
+ * scripts
  */
 
-jQuery(document).ready(function() {
-	
+jQuery(document).ready(function($){
+  $('#css-code').hide();
+  $('#css-btn2').hide();
+  $('#css-btn1').click(function(){
+    $('#css-code').show();
+    $('#css-btn2').show();
+    $('#css-btn1').hide();
+  });
+  $('#css-btn2').click(function(){
+    $('#css-code').hide();
+    $('#css-btn1').show();
+    $('#css-btn2').hide();
+  });
 });
