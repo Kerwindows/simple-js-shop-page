beb1ddc20a47f40ad8571fb8895ab6039a4021d9
Kerwin <kerjemtec@gmail.com>
2020-06-05 16:35:53 +0000

    Update css-columns.php

--- css-columns.php
+++ css-columns.php
@@ -15,7 +15,7 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 );
 
 //Optional: If you're using a private repository, specify the access token like this:
-$myUpdateChecker->setAuthentication('vjqhtoeCjpwq5aXaKzqD');
+$myUpdateChecker->setAuthentication('N5waYpiy49EFG6ii3pS2');
 
 //Optional: Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('master');
